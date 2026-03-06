9743179f23e521a96dbd3e321cb519e64d732844
tysp762-droid <tysp762@gmail.com>
2026-03-06 10:30:32 +0100

    talk scene

---
+++ My project/Assets/Scripts/UI/YapManeger.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class YapManeger : MonoBehaviour
+{
+    [SerializeField] private SpeechTest speechBubble; // Verwijzing naar jouw SpeechTest script
+
+    private string[] dialogueLines = {
+        "Arr... ye found me...",
+        "Good... good...",
+        "Listen close, matey...",
+        "I ain't got much time...",
+        "The ship... she is gone...",
+        "To pieces... but there  is hope...",
+        "Cough... groan..."
+    };
+
+    private int currentLine = 0;
+
+    void Start()
+    {
+        ShowNextLine();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShowNextLine();
+        }
+    }
+
+    public void ShowNextLine()
+    {
+        if (currentLine < dialogueLines.Length)
+        {
+            speechBubble.SetText(dialogueLines[currentLine]);
+            currentLine++;
+        }
+        else
+        {
+            speechBubble.SetText(""); // Verberg tekstwolk als dialoog klaar is
+        }
+    }
+}
+
