08828dfab23735b055db27da171ad04cb7b331ab
Qiyuri <Qiyuriamatsoehoet@gmail.com>
2026-03-16 10:36:36 +0100

    Inventory dragging
    ye

--- My project/Assets/Scripts/Inventory System/Scripts/Inventory.cs
+++ My project/Assets/Scripts/Inventory System/Scripts/Inventory.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
     public ItemsS0 rumItem;
     public ItemsS0 axeItem;
+
     public GameObject hotbarObj;
     public GameObject inventorySlotParent;
+    public GameObject container;
+
+    public Image dragIcon;
+
+    public float pickupRange = 3f;
+    private Item lookedAtItem = null;
+    public Material highlightMaterial;
+    private Material originalMaterial;
+    private Renderer lookedAtRenderer = null;
+
     private List<Slot> inventorySlots = new List<Slot>();
     private List<Slot> hotbarSlots = new List<Slot>();
     private List<Slot> allSlots = new List<Slot>();
+
+    private Slot draggedSlot;
+    private bool isDragging = false;
 
     private void Awake()
     {
@@ -23,14 +38,16 @@ public class Inventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.I))
+        if(Input.GetKeyDown(KeyCode.Tab))
         {
-            AddItem(rumItem, 3);
+            container.SetActive(!container.activeInHierarchy);
+            Cursor.lockState = Cursor.lockState == CursorLockMode.Locked ? CursorLockMode.None : CursorLockMode.Locked;
+            Cursor.visible = !Cursor.visible;
         }
-        else if(Input.GetKeyDown(KeyCode.O))
-        {
-            AddItem(axeItem, 1);
-        }
+
+        StartDrag();
+        UpdateDragItemPosition();
+        EndDrag();
     }
 
     public void AddItem(ItemsS0 itemToAdd, int amount)
@@ -76,5 +93,115 @@ public class Inventory : MonoBehaviour
         {
             Debug.Log("Inventory is full, could notadd all items. " + remaining + " of " + itemToAdd.itemName);
         }
+    }
+    
+    private void StartDrag()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Slot hovered = GetHoveredSlot();
+
+            if(hovered !=null && hovered.HasItem())
+            {
+                draggedSlot = hovered;
+                isDragging = true;
+
+                //show drag icon
+                dragIcon.sprite = hovered.GetItem().icon;
+                dragIcon.color = new Color(1, 1, 1, 0.5f);
+                dragIcon.enabled = true;
+            }
+        }
+    }
+
+    private void EndDrag()
+    {
+        if (Input.GetMouseButtonUp(0) && isDragging)
+        {
+            Slot hovered = GetHoveredSlot();
+
+            if(hovered != null)
+            {
+                HandleDrop(draggedSlot, hovered);
+
+                dragIcon.enabled = false;
+
+                draggedSlot = null;
+                isDragging = false;
+            }
+        }
+    }
+
+    private Slot GetHoveredSlot()
+    {
+        foreach (Slot s in allSlots)
+        {
+            if (s.hovering)
+            return s;
+        }
+
+        return null;
+    }
+
+    private void HandleDrop(Slot from, Slot to)
+    {
+        if (from == to) return;
+
+
+        //Stacking
+        if(to.HasItem() && to.GetItem() == from.GetItem())
+        {
+            int max = to.GetItem().maxStackSize;
+            int space = max - to.GetAmount();
+
+            if(space > 0)
+            {
+                int move = Mathf.Min(space, from.GetAmount());
+
+                to.SetItem(to.GetItem(), to.GetAmount() + move);
+                from.SetItem(from.GetItem(), from.GetAmount() - move);
+
+                if(from.GetAmount() <= 0)
+                    from.ClearSlot();
+                
+                return;
+            }
+        }
+
+        //Different Item
+        if (to.HasItem())
+        {
+            ItemsS0 tempItem = to.GetItem();
+            int tempAmount = to.GetAmount();
+
+            to.SetItem(from.GetItem(), from.GetAmount());
+            from.SetItem(tempItem, tempAmount);
+            return;
+        }
+
+        //Empty Slot
+        to.SetItem(from.GetItem(), from.GetAmount());
+        from.ClearSlot();
+    }
+
+    private void UpdateDragItemPosition()
+    {
+        if(isDragging)
+        {
+            dragIcon.transform.position = Input.mousePosition;
+        }
+    }
+
+    private void Pickup()
+    {
+            if(lookedAtRenderer != null && Input.GetKeyDown(KeyCode.E))
+            {
+                Item item = lookedAtRenderer.GetComponent<Item>();
+                if(item = null)
+                {
+                    AddItem(item.item, item.amount);
+                    Destroy(item.gameObject);
+                }
+            }
     }
 }
---
+++ My project/Assets/Scripts/Inventory System/Scripts/Item.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+public ItemsS0 item;
+public int amount = 1;
+}
